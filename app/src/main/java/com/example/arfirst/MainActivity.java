01c0c76e0a7dd3148d392a283b39b523fde69c64
Madiha Khan <reha.haniy04@gmail.com>
2023-08-20 14:06:27 +0500

    Initial Commit

---
+++ app/src/main/java/com/example/arfirst/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.arfirst;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.google.ar.sceneform.ux.ArFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArFragment arFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Load model.glb from assets folder or http url
+        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.arFragment);
+        assert arFragment != null;
+        arFragment.setOnTapPlaneGlbModel("model.glb");
+//        arFragment.setOnTapPlaneGlbModel("https://github.com/codemaker2015/3d-models/blob/master/glb_files/model.glb?raw=true");
+    }
+}
